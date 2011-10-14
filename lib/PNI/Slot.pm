4bade0ed1b825c55e6c0fcbcdf256ac7ef11be27
fibo <fibo@cpan.org>
2011-10-14 19:11:04 +0200

    slot types

--- lib/PNI/Slot.pm
+++ lib/PNI/Slot.pm
@@ -1,12 +1,39 @@
 package PNI::Slot;
 use PNI::Mo;
+use Scalar::Util;
 extends 'PNI::Elem';
 
 has box  => ( default => sub { PNI::Rectangle->new } );
 has data => ( default => sub { } );
 has node => ( default => sub { } );
 
+sub is_array { shift->type eq 'ARRAY' ? 1 : 0 }
+
+sub is_code { shift->type eq 'CODE' ? 1 : 0 }
+
+sub is_defined { shift->type eq 'UNDEF' ? 0 : 1 }
+
+sub is_hash { shift->type eq 'HASH' ? 1 : 0 }
+
+sub is_number { Scalar::Util::looks_like_number( shift->data ) ? 1 : 0 }
+
+sub is_scalar { shift->type eq 'SCALAR' ? 1 : 0 }
+
+sub is_string {
+    my $self = shift;
+    $self->is_scalar or return 0;
+    $self->is_number ? 0 : 1;
+}
+
+sub is_undef { shift->type eq 'UNDEF' ? 1 : 0 }
+
 sub translate { shift->box->translate(@_) }
+
+sub type {
+    my $data = shift->data;
+    defined $data or return 'UNDEF';
+    return ref $data || 'SCALAR';
+}
 
 1
 __END__
@@ -25,7 +52,28 @@ PNI::Slot - is a basic unit of data
 
 =head1 METHODS
 
+=head1 is_array
+
+=head1 is_code
+
+=head1 is_defined
+
+=head1 is_hash
+
+=head1 is_number
+
+=head1 is_scalar
+
+=head1 is_string
+
+=head1 is_undef
+
 =head2 translate
+
+=head2 type
+
+Returns a string representing the slot data type, i.e. UNDEF, SCALAR or the
+return value of C<ref> function.
 
 =cut
 
--- t/_slot.t
+++ t/_slot.t
@@ -1,10 +1,40 @@
 use strict;
 use warnings;
-use Test::More tests => 2;
+use Test::More tests => 16;
 use PNI::Slot;
 
 my $slot = PNI::Slot->new;
 isa_ok $slot, 'PNI::Slot';
 
 is $slot->data, undef, 'default data';
+
+is $slot->type, 'UNDEF', 'UNDEF type';
+ok $slot->is_undef, 'is_undef';
+
+$slot->data('scalar');
+is $slot->type, 'SCALAR', 'SCALAR type';
+ok $slot->is_scalar, 'is_scalar';
+ok $slot->is_string, 'is_string';
+
+$slot->data(1);
+ok $slot->is_number, 'is_number';
+
+$slot->data(0);
+is $slot->type, 'SCALAR', 'zero is a SCALAR';
+ok $slot->is_defined, 'is_defined';
+
+my $array = [];
+$slot->data($array);
+is $slot->type, ref $array, 'ARRAY type';
+ok $slot->is_array, 'is_array';
+
+my $hash = {};
+$slot->data($hash);
+is $slot->type, ref $hash, 'HASH type';
+ok $slot->is_hash, 'is_hash';
+
+my $code = sub { };
+$slot->data($code);
+is $slot->type, ref $code, 'CODE type';
+ok $slot->is_code, 'is_code';
 
