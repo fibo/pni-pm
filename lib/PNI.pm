10c8c937109687fee3666f9717ff1805a5b47cb4
fibo <casati_gianluca@yahoo.it>
2012-03-11 19:41:35 +0100

    ready for 0.29

--- lib/PNI.pm
+++ lib/PNI.pm
@@ -7,7 +7,7 @@ use warnings;
 # considering version x.yy stable, a new release will be versioned
 # as x.yy_zz so it will not be listed by CPAN.pm as the last.
 # After test results will be ok, it can be officially released x.(yy+1) version.
-our $VERSION = '0.28_01';
+our $VERSION = '0.29';
 $VERSION = eval $VERSION;
 
 use Exporter 'import';
